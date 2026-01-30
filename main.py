9ad88ac284848af83ae007f039621f541d5e39e2
arenkova17 <73025032+arenkova17@users.noreply.github.com>
2026-01-30 10:09:14 +0300

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 import uvicorn
 from fastapi.responses import HTMLResponse
-from database import get_all_klients, get_contract_id
+from database import get_all_klients, get_contract_id, update_contract
 from fastapi import HTTPException
+from fastapi import Form
 
 #главная страница с таблицей
 app = FastAPI()
@@ -70,12 +71,20 @@ def contract_page(contract_id: int):
         font-size: 18px; /* Размер текста */
         padding: 5px 20px; /* Поля вокруг текста */
         color: white;
-        text-decoration: none
+        text-decoration: none;
         }}
     .h1 {{
         background: #e4f0fb; /* Цвет фона под заголовком */
         color: #0952a0; /* Цвет текста */
         padding: 8px; /* Поля вокруг текста */
+    }}
+    .button-edit {{
+    border: 3px solid #1073b7; /* Параметры рамки */
+        background: #1073b7; /* Цвет фона */
+        font-size: 18px; /* Размер текста */
+        padding: 5px 20px; /* Поля вокруг текста */
+        color: white;
+        text-decoration: none;
     }}
     </style>
     </head>
@@ -84,13 +93,93 @@ def contract_page(contract_id: int):
         <p><strong>Дата договора:</strong> {contract.get('Дата начала', 'Нет данных')}</p>    
         <p><strong>Сумма договора:</strong> {contract.get('Сумма договора', 'Нет данных')} рублей</p>
         <p><strong>Предмет договора:</strong> {contract.get('Предмет договора', 'Нет данных')}</p>
+        
         <p style="position: fixed; bottom: 20px; left: 20px;">
         <a href="/" class="button-back"> Назад к списку</a>
         </p>
+        <p style="position:fixed; bottom: 20px; right: 20px">
+        <a href="/contract/{contract_id}/update" class="button-edit"> Изменить </a>
+        </p>
+        
+        
     </body>
     </html>
     """
     return html
+
+#функуция которая заполняет значения в изменении ( окно редактирования )
+@app.get("/contract/{contract_id}/update", response_class=HTMLResponse)
+def edit_contract_page(contract_id: int):
+    contract = get_contract_id(contract_id)
+    if not contract:
+        return "<h1>Договор не найден</h1>"
+
+    html = f"""
+    <html>
+    <head><title>Изменение данных</title>
+    <style>
+    .h1 {{
+        background: #e4f0fb; /* Цвет фона под заголовком */
+        color: #0952a0; /* Цвет текста */
+        padding: 8px; /* Поля вокруг текста */
+    }}
+    .button-edit {{   /*сохранить*/
+        border: 3px solid #1073b7; /* Параметры рамки */
+        background: #1073b7; /* Цвет фона */
+        font-size: 18px; /* Размер текста */
+        padding: 5px 20px; /* Поля вокруг текста */
+        color: white;
+        text-decoration: none;
+        }}
+    .button-back {{    /*назад к просмотру*/
+        border: 3px solid #1073b7; /* Параметры рамки */
+        background: #1073b7; /* Цвет фона */
+        font-size: 18px; /* Размер текста */
+        padding: 5px 20px; /* Поля вокруг текста */
+        color: white;
+        text-decoration: none;
+    }}
+    </style>
+    </head>
+    <body>
+    <h1 class="h1">Изменение данных договора {contract_id}</h1>
+    <form action="/api/contract/{contract_id}/update" method="POST">
+        <label>Дата договора:</label>
+            <input type="date" name="dn" value="{contract.get('Дата начала', '')}"><br><br>
+        <label>Сумма договора:</label>
+            <input type="number" name="summa" value="{contract.get('Сумма договора', 0)}"><br><br>
+        <label>Предмет договора:</label>
+            <input type="text" name="predmet" value="{contract.get('Предмет договора', '')}"><br><br>
+        <p style="position: fixed; bottom: 20px; right: 20px;">
+        <a href="#" onclick="this.closest('form').submit(); return false;" class="button-edit">Сохранить</a>
+    </form>
+    <p style="position: fixed; bottom: 20px; left: 20px;">
+    <a href="/contract/{contract_id}" class="button-back">Назад к просмотру</a></p>
+    
+    </body>
+    </html>
+    """
+    return html
+
+#функция обновления данных в базе
+@app.post("/api/contract/{contract_id}/update")
+def update_contract_api(
+        contract_id: int,
+        dn: str = Form(...),
+        summa: float = Form(...),
+        predmet: str = Form(...)):
+    success = update_contract(contract_id, dn, summa, predmet)
+
+    if success:
+        # Возвращаем обратно на страницу договора
+        return HTMLResponse(f"""
+            <script>
+                alert('Договор обновлён!');
+                window.location.href = '/contract/{contract_id}';
+            </script>
+        """)
+    else:
+        raise HTTPException(500, "Ошибка при обновлении")
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="127.0.0.1", port=8000)
