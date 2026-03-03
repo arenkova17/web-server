5ad9dfff7e3c585a304a64cbfd019841393c9a4f
arenkova17 <73025032+arenkova17@users.noreply.github.com>
2026-03-03 15:35:26 +0300

    database.py

--- database.py
+++ database.py
@@ -1,21 +1,37 @@
 import pyodbc
-import os
+import winrm    #библиотека для работы с WinRM - протоколом удаленного управления Windows
+
 # подключение к бд
 driver = '{ODBC Driver 17 for SQL Server}'
 server = 'gazprosql'
 database = 'tmp_dog'
 connection_string = f'DRIVER={driver};SERVER={server};DATABASE={database};Trusted_Connection=yes'
 
-#функция вывода таблицы почастично ( первые 4000 клиентов. вторые 4000 клиентов и тд)
-def get_clients_page(page: int = 1, page_size: int = 4000):
+#создает подключение к серверу от имени пользователя из сессии
+def get_user_connection(request):
+    user = request.session.get("user")
+    if not user:
+        return None
+
+    conn_str = (
+        f'DRIVER={driver};'
+        f'SERVER={server};'
+        f'DATABASE={database};'
+        f'UID={user["login"]};'
+        f'Trusted_Connection=yes;'
+    )
+    return pyodbc.connect(conn_str)
+
+# функция вывода таблицы почастично ( первые 4000 клиентов. вторые 4000 клиентов и тд)
+def get_clients_page(request, page: int = 1, page_size: int = 4000):
     try:
-        connection = pyodbc.connect(connection_string)
+        connection = get_user_connection(request)
         if not connection:
             return []
-        cursor = connection.cursor()   #курсор для выполнения sql запросов
+        cursor = connection.cursor()  # курсор для выполнения sql запросов
 
         # Рассчитываем offset (колво строк которые будем пропускать)
-        offset = (page - 1) * page_size   #(1-1)*4000=0 (1 стр. пропускаем 0 строк), (2-1)*4000=4000 (2стр. пропукаем 4000 строк) и тд
+        offset = (page - 1) * page_size  # (1-1)*4000=0 (1 стр. пропускаем 0 строк), (2-1)*4000=4000 (2стр. пропукаем 4000 строк) и тд
 
         cursor.execute(f"""
             SELECT 
@@ -32,25 +48,27 @@ def get_clients_page(page: int = 1, page_size: int = 4000):
             FETCH NEXT {page_size} ROWS ONLY
         """)
 
-        columns = [column[0] for column in cursor.description]   #получение названия заголовков
-        result = []   #создание списка
+        columns = [column[0] for column in cursor.description]  # получение названия заголовков
+        result = []  # создание списка
 
-        for row in cursor.fetchall():    #fetchall - берет все строки. fetchone - получение первой строки
-            row_dict = dict(zip(columns, row))    #zip(columns, row) получает сначала строку с заголовками, потом строку с его значением и соединяет их, а dict преобразует каждую такую строку в словарь типа  {'ID договора': 8001, '№ контрагента': '123', 'Дата начала': '2023-01-01', ...},
-            result.append(row_dict)    #заносит результат в список
+        for row in cursor.fetchall():  # fetchall - берет все строки. fetchone - получение первой строки
+            row_dict = dict(zip(columns,
+                                row))  # zip(columns, row) получает сначала строку с заголовками, потом строку с его значением и соединяет их, а dict преобразует каждую такую строку в словарь типа  {'ID договора': 8001, '№ контрагента': '123', 'Дата начала': '2023-01-01', ...},
+            result.append(row_dict)  # заносит результат в список
 
         cursor.close()
-        connection.close()   #закрывается соединение
-        return result    #возвращается результат
+        connection.close()  # закрывается соединение
+        return result  # возвращается результат
 
-    except Exception as e:      #вывод ошибки и пустого списка в случае краха
+    except Exception as e:  # вывод ошибки и пустого списка в случае краха
         print(f'Error in get_clients_page: {e}')
         return []
 
-#функция для добычи одного договора по id, нужна для окошка с инфой клиента
-def get_contract_id(contract_id: int):
+
+# функция для добычи одного договора по id, нужна для окошка с инфой клиента
+def get_contract_id(request, contract_id: int):
     try:
-        connection = pyodbc.connect(connection_string)
+        connection = get_user_connection(request)
         if not connection:
             return []
         cursor = connection.cursor()
@@ -73,7 +91,7 @@ def get_contract_id(contract_id: int):
                 klint.KPP as 'КПП',
                 klint.tel as 'Телефон/факс',
                 dog.konk as 'Конкурс',
-                
+
                 dog.sposobzak as 'Способ закупки',
                 dog.VIdZAK as 'Вид закупки',
                 dog.numzak as 'Номер КЗ',
@@ -84,7 +102,7 @@ def get_contract_id(contract_id: int):
                 dog.OSTNEKONZAK as 'Код основания',
                 dog.okpd2 as 'ОКПД2',
                 dog.subectzak as 'Субъектзак',
-                
+
                 dog_okz.num_z  as '№ закупки ЕИС', 
                 dog_okz.num_z_el as '№ закупю на эл.пл.',
                 dog_okz.pr_z as 'Прямая закупка',
@@ -92,7 +110,7 @@ def get_contract_id(contract_id: int):
                 dog_okz.gpz as 'ГПЗ',
                 dog_okz.UID_dog_ as 'UID',
                 dog_okz.ppz as 'ППЗ',
-                
+
                 dog.prol as 'Пролонгация',
                 dog.beznds as 'Без НДС',
                 klint.asbu as 'Код ОБД НСИ',
@@ -108,30 +126,30 @@ def get_contract_id(contract_id: int):
         """, contract_id)
 
         columns = [column[0] for column in cursor.description]
-        row = cursor.fetchone()   #получение строки
+        row = cursor.fetchone()  # получение строки
 
         cursor.close()
         connection.close()
-        #если строка получена, возвращаем её в виде {'id': 57, 'Дата начала': '2023-01-01', ...}
+        # если строка получена, возвращаем её в виде {'id': 57, 'Дата начала': '2023-01-01', ...}
         if row:
             return dict(zip(columns, row))
-        else: return None
+        else:
+            return None
 
     except Exception as e:
         print(f'Error: {e}')
         return None
 
-#возвращение всех всех клиентов (нужно для просчета количества выводимых клиентов)
-def get_total_count():
-    """Возвращает общее количество договоров в таблице"""
+# считает всех всех клиентов (нужно для просчета количества выводимых клиентов)
+def get_total_count(request):
     try:
-        connection = pyodbc.connect(connection_string)
+        connection = get_user_connection(request)
         if not connection:
             return []
         cursor = connection.cursor()
 
-        cursor.execute("SELECT COUNT(*) FROM dog")   #подсчет количества строк в таблице
-        count = cursor.fetchone()[0]   #выводит первой строк - цифры количества строк
+        cursor.execute("SELECT COUNT(*) FROM dog")  # подсчет количества строк в таблице
+        count = cursor.fetchone()[0]  # выводит первой строк - цифры количества строк
 
         cursor.close()
         connection.close()
@@ -141,10 +159,13 @@ def get_total_count():
         print(f'Error in get_total_count: {e}')
         return 0
 
-#обновление полей и чекбоксов
-def update_par(contract_id: int, konk: int, prol: int, beznds: int, opl: int, eis: int, statusD: int, d_end: str, sposobzak: str, VIdZAK: int, numzak: str, predlog: int, dat_docosznak: str, num_docosnzak: int, smsp: int, OSTNEKONZAK: int, okpd2: int, subectzak: int, num_z: str, num_z_el: str, pr_z: int, pr_z_osn: str, gpz: str, uid: str, ppz: str):
+# обновление полей и чекбоксов
+def update_par(request, contract_id: int, konk: int, prol: int, beznds: int, opl: int, eis: int, statusD: int,
+               d_end: str, sposobzak: str, VIdZAK: int, numzak: str, predlog: int, dat_docosznak: str,
+               num_docosnzak: int, smsp: int, OSTNEKONZAK: int, okpd2: int, subectzak: int, num_z: str, num_z_el: str,
+               pr_z: int, pr_z_osn: str, gpz: str, uid: str, ppz: str):
     try:
-        connection = pyodbc.connect(connection_string)
+        connection = get_user_connection(request)
         if not connection:
             return []
         cursor = connection.cursor()
@@ -158,7 +179,7 @@ def update_par(contract_id: int, konk: int, prol: int, beznds: int, opl: int, ei
             eis = ?,
             statusD = ?,
             d_end = ?,
-            
+
             sposobzak = ?,
             VIdZAK = ?,
             numzak = ?,
@@ -170,7 +191,8 @@ def update_par(contract_id: int, konk: int, prol: int, beznds: int, opl: int, ei
             okpd2 = ?,
             subectzak = ?
             WHERE id = ?
-        """, konk, prol, beznds, opl, eis, statusD, d_end, sposobzak, VIdZAK, numzak, predlog, dat_docosznak, num_docosnzak, smsp, OSTNEKONZAK, okpd2, subectzak,  contract_id)
+        """, konk, prol, beznds, opl, eis, statusD, d_end, sposobzak, VIdZAK, numzak, predlog, dat_docosznak,
+                       num_docosnzak, smsp, OSTNEKONZAK, okpd2, subectzak, contract_id)
 
         cursor.execute("""
             UPDATE dog_okz 
@@ -193,10 +215,11 @@ def update_par(contract_id: int, konk: int, prol: int, beznds: int, opl: int, ei
         print(f'Update error: {e}')
         return False
 
-#функция поиска 1 или несколько договоров после нажатия кнопки найти на диалоговом окне
-def search_dog(numberdog: str = "", numberkontr: str = ""):
+
+# функция поиска 1 или несколько договоров после нажатия кнопки найти на диалоговом окне
+def search_dog(request, numberdog: str = "", numberkontr: str = ""):
     try:
-        connection = pyodbc.connect(connection_string)
+        connection = get_user_connection(request)
         if not connection:
             return []
         cursor = connection.cursor()
@@ -267,10 +290,10 @@ def search_dog(numberdog: str = "", numberkontr: str = ""):
         print(f'Search error: {e}')
         return []
 
-#функция берёт процедуру и вытаскивает результат процедуры
-def get_dog_payments(contract_id: int):
+# функция берёт процедуру и вытаскивает результат процедуры (для таблицы оплат)
+def get_dog_payments(request, contract_id: int):
     try:
-        connection = pyodbc.connect(connection_string)
+        connection = get_user_connection(request)
         if not connection:
             return []
         cursor = connection.cursor()
@@ -293,5 +316,42 @@ def get_dog_payments(contract_id: int):
         print(f'Error getting payments: {e}')
         return []
 
+#проверка логина и пароля, выполнение команды на удаленном компе и возвращения true/false в результате, показывая подключилось или нет
+def verify_windows_login(host, username, password):
+    try:
+       #создаем сессию с подключением через winrm с логином и паролем и протоколом ntlm (используется для проверки подлинности пользователей для доступа к ресурсам на windows компе)
+        session = winrm.Session(
+            f'http://{host}:5985/wsman', #стандартный HTTP порт (5985) для управравления удаленным виндовс компом
+            auth=(username, password),
+            transport='ntlm'   #протокол
+        )
+        #пробуем выполнить команду 'hostname' для проверки - команда получения имени компьютера
+        result = session.run_cmd('hostname')
 
+        #если команда выполнилась без ошибок (код 0), значит аутентификация пройдена
+        if result.status_code == 0:
+            print(f"Успешный вход для {username}")
+            return True
+        else:
+            print(f"Ошибка выполнения команды для {username}: {result.std_err.decode('cp866', errors='ignore')}")
+            return False
 
+    except winrm.exceptions.InvalidCredentialsError:
+        print(f"Неверный логин или пароль для {username}")
+        return False
+    except Exception as e:
+        #ловим любые другие ошибки
+        print(f"Ошибка подключения для {username}: {e}")
+        return False
+
+#функция получения отдела зашедшего пользователя для того чтобы вывести только те договора которые относятся к его отделу
+def get_user_otd(username: str):
+    try:
+        conn = pyodbc.connect(connection_string)
+        cursor = conn.cursor()
+        cursor.execute("SELECT otd FROM dog_ident WHERE login = ?", username)
+        row = cursor.fetchone()
+        conn.close()
+        return row[0] if row else None
+    except:
+        return None
